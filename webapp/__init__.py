64a0dfda6aef7fd30cc67b414854cd603659a2ea
Yury Borodich <xkxixnxgx@yandex.ru>
2020-03-19 20:51:51 +0300

    Отработал конфликты при слиянии веток. Удалил тестовые файлы.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template
-<<<<<<< HEAD
-# импортируем структуру базы данных
-from webapp.model import db
-=======
 from webapp.forms import LoginForm
 # импортируем структуру базы данных
 from webapp.model import db
 from webapp.config import SECRET_KEY
->>>>>>> 8084cea60ac5e5fbbcce3f52fe2af1890eb9fd6a
+
 
 def create_app():
     app = Flask(__name__)
@@ -22,15 +18,13 @@ def create_app():
     def about():
         return render_template('about.html')
 
-<<<<<<< HEAD
-=======
+
     @app.route('/login')
     def login():
         title = 'Sigh in'
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
 
->>>>>>> 8084cea60ac5e5fbbcce3f52fe2af1890eb9fd6a
     if __name__ == '__main__':
         app.run(debug=True)
 
