e634d16b25295aac511225d8850934bc1a51bdfb
Yury Borodich <xkxixnxgx@yandex.ru>
2020-04-30 10:49:46 +0300

    Внесены изменения в модели, написаны некоторые роуты к блюпринту tracks. Удален файл базы данных, удалены миграции. Не создается база заново.

--- webapp/tracks/forms.py
+++ webapp/tracks/forms.py
@@ -12,22 +12,24 @@ class TrackForm(FlaskForm):
                              validators=[DataRequired()],
                              render_kw={"class": "progress-bar"}
                              )
+
     def validate_size_time(self, user_email):
         size_now = Track.query.filter_by(track_name=track_name.data).sum()
         if  100 < size_now < 0:
             raise ValidationError('Error is the accumulated time.')
 
+
 class TrackAddForm(FlaskForm):
     # render_kw параметр в wtf, в котором можно указать то, что будет добавлено при отрисовке формы.
     # Мы добавляем класс из bootstrap из строки input class="form-control"
-    track_name = StringField('Your track', render_kw={"class": "form-control"})
-    total_time = StringField('Time, hours', render_kw={"class": "form-control"})
+    track_name = StringField('New name of the track', render_kw={"class": "form-control"})
+    total_time = StringField('Time, hours of the track', render_kw={"class": "form-control"})
     track_add = SubmitField('Add track', render_kw={"class": "btn btn-success"})
 
 
 class TrackEditForm(FlaskForm):
-    track_name = StringField('Your track', render_kw={"class": "form-control"})
-    total_time = StringField('Time, hours', render_kw={"class": "form-control"})
+    track_name = StringField('Edit your track', render_kw={"class": "form-control"})
+    total_time = StringField('New time, hours', render_kw={"class": "form-control"})
     save_edit = SubmitField('Save', render_kw={"class": "btn btn-success"})
 
 
@@ -36,7 +38,6 @@ class PointForm(FlaskForm):
     track_name = StringField('Track name', render_kw={"class": "table"})
     spent_time = StringField('Spent time', render_kw={"class": "table"})
     comment = StringField('Comment', render_kw={"class": "overflow-auto"})
-
 
 
 class PointAddForm(FlaskForm):
--- webapp/tracks/views.py
+++ webapp/tracks/views.py
@@ -1,34 +1,35 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask import abort, Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user
-from webapp.tracks.forms import TrackForm, TrackAddForm, PointAddForm
+from webapp.tracks.forms import TrackForm, TrackAddForm, PointForm, PointAddForm
 from webapp.tracks.models import Track, Point
+from webapp.user.models import User
 from datetime import datetime
 
 from webapp import db
 
 # в данном случае необходимо импортировать модель Track, т.к. при миграции базы данных
 # migrate смотрит на подключенные blueprints, т.е. по факту на файлы views и импортирует модели для миграции
-from webapp.tracks.models import Track
 
 blueprint = Blueprint('tracks', __name__,  url_prefix='/tracks')
 
 
-@blueprint.route('/')
+@blueprint.route('/', method=['GET'])
 def tracks_points():
     tittle = 'Tracks'
     tracks_form = TrackForm()
-    return render_template('tracks/tracks.html', page_title=tittle, form=tracks_form)
+    point_form = PointForm()
+    return render_template('tracks/tracks.html', page_title=tittle, form=tracks_form, point_form=point_form)
 
 
 @blueprint.route('/track_add', methods=['POST'])
 def track_add():
-    tracks_form = TrackAddForm()
-    if tracks_form.validate_on_submit():
+    track_add_form = TrackAddForm()
+    if track_add_form.validate_on_submit():
         date_now = datetime.now()
         date_reg = date_now.strftime('%d.%m.%Y')
         new_track = Track(date_add=date_reg,
-                          track_name=tracks_form.track_name.data,
-                          total_time=tracks_form.total_time.data,
+                          track_name=track_add_form.track_name.data,
+                          total_time=track_add_form.total_time.data,
                           time_sum=0,
                           )
         db.session.add(new_track)
@@ -36,9 +37,9 @@ def track_add():
         flash('You added the new track.')
         return redirect(url_for('tracks.tracks_points'))
     else:
-        for field, errors in tracks_form.errors.items():
+        for field, errors in track_add_form.errors.items():
             for error in errors:
-                flash(f"Error in the field {getattr(tracks_form, field).label.text}: {error}")
+                flash(f"Error in the field {getattr(track_add_form, field).label.text}: {error}")
         return redirect(url_for('tracks.tracks_points'))
 
 
@@ -56,9 +57,31 @@ def track_del():
     return redirect(url_for('tracks.tracks_points'))
 
 
-@blueprint.route('/analytics')
-def analytics():
+@blueprint.route('/points_add', methods=['POST'])
+def point_add():
+    point_add_form = PointAddForm()
+    if point_add_form.validate_on_submit():
+        date_now = datetime.now()
+        new_point = Point(date_add=date_now,
+                          time_value=point_add_form.time_value.data,
+                          comment=point_add_form.comment.data,
+                          )
+        db.session.add(new_point)
+        db.session.commit()
+        flash('You added the new point.')
+        return redirect(url_for('tracks.tracks_points'))
+    else:
+        for field, errors in point_add_form.errors.items():
+            for error in errors:
+                flash(f"Error in the field {getattr(point_add_form, field).label.text}: {error}")
+        return redirect(url_for('tracks.tracks_points'))
+
+
+@blueprint.route('/analytics/<int:track_id>')
+def analytics(track_id):
     title = 'Analytics'
     tracks_form = TrackForm()
-    points_form = Point
-    return render_template('tracks/analytics.html', page_title=title, form=tracks_form)
+    my_track = Track.query.filter(Track.id == track_id).first()
+    if not my_track:
+        abort(404)
+    return render_template('tracks/analytics.html', page_title=title, form=tracks_form, track=my_track)
